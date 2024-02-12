14c877ea71c874159eb7c84a5e4cdc5bd0d67b98
damoblinkz2 <adedamolaolatomide@gmail.com>
2024-02-12 23:22:17 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="nav">
+          <ul>
+            <li>Home</li>
+            <li>News</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+      <div className="container"></div>
+      <footer className="footer">@2024</footer>
+    </>
+  );
+};
+
+export default App;
