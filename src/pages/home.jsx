56703d2e3181cc90e86861b3f2f70d277137dbb0
sammyhak <samsonbaruwa6@gmail.com>
2024-02-13 01:47:28 +0100

    chores

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,21 @@
 import "../styles/pages/home.scss";
+import searchIcon from "../assets/icons/searchicon.svg";
 
 const Home = () => {
   return (
     <>
       <section className="home">
         <div className="container">
-          <div className="hero-section"></div>
+          <div className="hero-section">
+            Experience More, Engage More Vote & Watch Live!
+          </div>
+
+          <div className="search-container">
+            <span className="search-icon">
+                <img src={searchIcon} alt="searchIcon" />
+            </span>
+            <input type="text" name="search" id="search" placeholder="search" />
+          </div>
         </div>
       </section>
     </>
