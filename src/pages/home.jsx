0c1e940dc7c35eac7d7321a20c498e1ad0614b63
sammyhak <samsonbaruwa6@gmail.com>
2024-02-13 02:50:15 +0100

    chores

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,8 @@
 import "../styles/pages/home.scss";
 import searchIcon from "../assets/icons/searchicon.svg";
+import voteIcon from "../assets/icons/vote.svg";
+import streamIcon from "../assets/icons/stream.svg";
+import spinIcon from "../assets/icons/spin.svg";
 
 const Home = () => {
   return (
@@ -15,6 +18,37 @@ const Home = () => {
                 <img src={searchIcon} alt="searchIcon" />
             </span>
             <input type="text" name="search" id="search" placeholder="search" />
+          </div>
+
+          <div className="features">
+            <div className="features-container">
+                <div className="feature">
+                    <div>
+                        <img src={voteIcon} alt="voteIcon" />
+                    </div>
+                    <div className="feature-text">
+                        Vote
+                    </div>
+                </div>
+
+                <div className="feature">
+                    <div>
+                        <img src={streamIcon} alt="streamIcon" />
+                    </div>
+                    <div className="feature-text">
+                        Stream
+                    </div>
+                </div>
+
+                <div className="feature">
+                    <div>
+                        <img src={spinIcon} alt="spinIcon" />
+                    </div>
+                    <div className="feature-text">
+                        Spin
+                    </div>
+                </div>
+            </div>
           </div>
         </div>
       </section>
